ff05808886d7fc09fea4934c55a377f5dba44fbd
doraemon500 <pkt45349048@gmail.com>
2024-05-28 01:45:48 +0900

    init

---
+++ model_api.py
@@ -0,0 +1,35 @@
+import os
+import torch
+from fastapi import FastAPI
+from fastapi import UploadFile
+from transformers import ViTForImageClassification, ViTFeatureExtractor, pipeline
+from PIL import Image as PILImage
+
+os.environ["TRANSFORMERS_CACHE"] = "./cache/"
+os.environ["HF_HOME"] = "./cache/"
+
+MODEL_ID = ''
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = ViTForImageClassification.from_pretrained(MODEL_ID).to(device)
+feature_extractor = ViTFeatureExtractor.from_pretrained(MODEL_ID)
+
+app = FastAPI(
+  title="Model Server",
+  version="1.0",
+  description="A simple API server using huggingface's Runnable interfaces",
+)
+
+@app.post("/which_pokemon")  
+async def upload_photo(file: UploadFile): 
+    img = await file.read()
+    img = PILImage.open(io.BytesIO(img))
+    img = img.convert('RGB')
+    extracted = feature_extractor(images=img, return_tensors='pt').to(device)
+    predicted_id = model(**extracted).logits.argmax(-1).item()
+    return {"message": predicted_id}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
