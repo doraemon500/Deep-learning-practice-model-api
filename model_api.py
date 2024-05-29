1f99a430dcac31a8e661473ad9114a0e3f35e2a3
doraemon500 <pkt45349048@gmail.com>
2024-05-29 18:08:46 +0900

    add predcited_score to API Response

--- model_api.py
+++ model_api.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi import UploadFile
 from transformers import ViTForImageClassification, ViTFeatureExtractor, pipeline
 from PIL import Image as PILImage
+import torch.nn.functional as F
 
 os.environ["TRANSFORMERS_CACHE"] = "./cache/"
 os.environ["HF_HOME"] = "./cache/"
@@ -22,14 +23,21 @@ app = FastAPI(
 )
 
 @app.post("/which_pokemon")  
-async def upload_photo(file: UploadFile): 
+async def upload_photo(file: UploadFile):
     img = await file.read()
+    # print(file.filename)
     img = PILImage.open(io.BytesIO(img))
     img = img.convert('RGB')
     extracted = feature_extractor(images=img, return_tensors='pt').to(device)
-    predicted_id = model(**extracted).logits.argmax(-1).item()
+
+    predicted_logits = model(**extracted).logits
+    predicted_id = predicted_logits.argmax(-1).item()
+    softmax_probs = F.softmax(predicted_logits, dim=1)
+    predicted_score, _ = torch.max(softmax_probs, dim=1)
+    predicted_score = predicted_score.item() 
     predicted_pokemon = model.config.id2label[predicted_id]
-    return {"message": predicted_pokemon}
+
+    return {"pokemon": predicted_pokemon, "score": predicted_score}
 
 
 if __name__ == "__main__":
